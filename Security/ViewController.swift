0fa5b7a0b91b30c71cef33f4e0484d6b10c7aabc
Artyom Abramov <artemiiab@gmail.com>
2020-12-23 18:43:54 +0400

    Initial Commit

---
+++ Security/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Security
+//
+//  Created by Артём Абрамов on 23.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
